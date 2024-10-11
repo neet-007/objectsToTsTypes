c68515bf29ae8243d62cfcf8049bb47ec8a7e225
neet-007 <moayedeffect@gmail.com>
2024-10-12 00:31:26 +0300

    split the app to functions and made simple array convertion

--- main.go
+++ main.go
@@ -6,16 +6,31 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"strings"
 )
 
-var types map[string]string
+var Types map[string]string
 
 func init() {
-	types = map[string]string{
-		"string":  "string",
-		"int":     "number",
-		"float64": "number",
-		"bool":    "boolean",
+	Types = map[string]string{
+		"string":         "string",
+		"int":            "number",
+		"int8":           "number",
+		"int16":          "number",
+		"int32":          "number",
+		"int64":          "number",
+		"uint":           "number",
+		"uint8":          "number",
+		"uint16":         "number",
+		"uint32":         "number",
+		"uint64":         "number",
+		"float32":        "number",
+		"float64":        "number",
+		"bool":           "boolean",
+		"byte":           "number",
+		"rune":           "string",
+		"interface{}":    "any",
+		"[]interface {}": "any []",
 	}
 }
 func main() {
@@ -29,6 +44,17 @@ func main() {
 		log.Fatal("couldn not unmarshal json")
 	}
 
+	types := convertTypes(unmarshaledJson)
+	fmt.Println(types)
+
+	data := formatTypes(types)
+	err = os.WriteFile("ts_types.ts", data, 0666)
+	if err != nil {
+		log.Fatal("could not write to file")
+	}
+}
+
+func convertTypes(unmarshaledJson map[string]*json.RawMessage) map[string]string {
 	fmt.Println("the json")
 	for key, val := range unmarshaledJson {
 		fmt.Printf("key: %s, val: %s\n", key, val)
@@ -45,9 +71,51 @@ func main() {
 			continue
 		}
 
-		tsType[key] = types[reflect.TypeOf(temp).String()]
+		fmt.Printf("type of temp:%v -> %v\n", temp, reflect.TypeOf(temp).String())
+
+		if reflect.TypeOf(temp).String() == "[]interface {}" {
+			convertArray(val)
+		}
+
+		tsType[key] = Types[reflect.TypeOf(temp).String()]
 	}
 
+	return tsType
+}
+
+func convertStruct() {
+
+}
+
+func convertArray(arr *json.RawMessage) {
+	var temp []*json.RawMessage
+	err := json.Unmarshal(*arr, &temp)
+	if err != nil {
+		fmt.Println("could not unmarshal json arr")
+		return
+	}
+
+	arrTypes := make([]string, len(temp))
+
+	for _, val := range temp {
+		var tempVal interface{}
+		err = json.Unmarshal(*val, &tempVal)
+		if err != nil {
+			fmt.Printf("could not unmarshal json val %v\n", val)
+			return
+		}
+
+		arrTypes = append(arrTypes, Types[reflect.TypeOf(tempVal).String()])
+
+	}
+
+	arrTypes = removeDuplicates(arrTypes)
+
+	arrWithTypes := strings.Trim(fmt.Sprintf("%s []", strings.Join(arrTypes, " | ")), " | ")
+	fmt.Printf("\n\n%s\n\n", arrWithTypes)
+}
+
+func formatTypes(types map[string]string) []byte {
 	data := []byte{}
 	for _, c := range "type NewType = {" {
 		data = append(data, byte(c))
@@ -55,7 +123,7 @@ func main() {
 	data = append(data, '\n')
 
 	fmt.Println("the type")
-	for key, val := range tsType {
+	for key, val := range types {
 		fmt.Printf("key: %s, val: %s\n", key, val)
 
 		for _, c := range key {
@@ -69,9 +137,18 @@ func main() {
 	}
 	data = append(data, '}')
 
-	err = os.WriteFile("ts_types.ts", data, 0666)
-	if err != nil {
-		log.Fatal("could not write to file")
-	}
+	return data
+}
 
+func removeDuplicates(slice []string) []string {
+	seen := make(map[string]bool)
+	result := []string{}
+
+	for _, val := range slice {
+		if _, ok := seen[val]; !ok {
+			seen[val] = true
+			result = append(result, val)
+		}
+	}
+	return result
 }
